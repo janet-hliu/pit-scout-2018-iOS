40a9c977c55e70a77b8b783d91a0458933e70b55
Janet Liu <jliu@Janets-MacBook-Pro.local>
2017-03-16 19:38:30 -0700

    fixed missing data crash

--- DropboxPhotoTest/MissingDataViewController.swift
+++ DropboxPhotoTest/MissingDataViewController.swift
@@ -12,7 +12,7 @@ import Firebase
 class MissingDataViewController : UIViewController {
     @IBOutlet weak var mdTextView: UITextView!
     /// Teams Firebase Snapshot
-    var snap : FIRDataSnapshot? = nil {
+    var snap : FIRDataSnapshot? = nil{
         didSet {
             self.viewDidLoad()
         }
@@ -31,17 +31,19 @@ class MissingDataViewController : UIViewController {
         if let snap = self.snap {
             for team in snap.children.allObjects {
                 let t = (team as! FIRDataSnapshot).value as! [String: AnyObject]
-                if t["pitSelectedImageName"] == nil {
-                    self.updateWithText("\nTeam \(t["number"]!) has no selected image name.", color: UIColor.blue)
-                }
-                var dataNils : [String] = []
-                for key in self.firebaseKeys {
-                    if t[key] == nil && !self.ignoreKeys.contains(key) {
-                        dataNils.append(key)
+                if t["number"] != nil {
+                    if t["pitSelectedImageName"] == nil {
+                        self.updateWithText("\nTeam \(t["number"]!) has no selected image name.", color: UIColor.blue)
                     }
-                }
-                for dataNil in dataNils {
-                    self.updateWithText("\nTeam \(t["number"]!) is missing datapoint: \(dataNil).", color: UIColor.orange)
+                    var dataNils : [String] = []
+                    for key in self.firebaseKeys {
+                        if t[key] == nil && !self.ignoreKeys.contains(key) {
+                            dataNils.append(key)
+                        }
+                    }
+                    for dataNil in dataNils {
+                        self.updateWithText("\nTeam \(t["number"]!) is missing datapoint: \(dataNil).", color: UIColor.orange)
+                    }
                 }
             }
         }
